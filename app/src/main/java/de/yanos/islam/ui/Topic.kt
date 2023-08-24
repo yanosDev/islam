4ab0b376bee77fd315796de2f44d21864ae21de7
yanosDev <35569647+yanosDev@users.noreply.github.com>
2023-08-24 19:29:12 +0200

    1
    Finalize style of topic screen

--- app/src/main/java/de/yanos/islam/ui/Topic.kt
+++ app/src/main/java/de/yanos/islam/ui/Topic.kt
@@ -1,37 +1,37 @@
-package de.yanos.islam.ui
+@file:OptIn(ExperimentalFoundationApi::class)
 
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
+package de.yanos.islam.ui
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.add
-import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Divider
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import de.yanos.islam.R
-import de.yanos.islam.util.IslamDivider
+import de.yanos.islam.data.model.Topic
+import de.yanos.islam.util.PatternedBackgroung
 
 @Preview
 @Composable
@@ -40,31 +40,56 @@ fun TopicView(
     vm: TopicViewModel = hiltViewModel(),
     onTopicSelected: (String) -> Unit = {}
 ) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(8.dp)
-    ) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            LazyColumn(
-                modifier = Modifier
-                    .wrapContentHeight()
-                    .clip(RoundedCornerShape(16.dp)),
-                contentPadding = WindowInsets.statusBars.add(WindowInsets(top = 48.dp)).asPaddingValues(),
-            ) {
-                items(
-                    items = vm.state,
-                    key = { it.id }
-                ) { topic ->
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(text = topic.title, style = MaterialTheme.typography.headlineMedium)
-                    Spacer(modifier = Modifier.height(4.dp))
-                    IslamDivider()
-                    Spacer(modifier = Modifier.height(4.dp))
-                }
-            }
-          //  Image(painter = painterResource(id = R.drawable.bg_1), contentDescription = "anything")
+    PatternedBackgroung(modifier = modifier) {
+        Column(modifier = Modifier.padding(top = 32.dp)) {
+            HeaderStars()
+            TopicHeader(modifier = Modifier.align(Alignment.CenterHorizontally))
+            TopicList(modifier = Modifier.align(Alignment.CenterHorizontally), topics = vm.state)
+        }
+    }
+}
+@Composable
+fun HeaderStars(modifier: Modifier = Modifier) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.stars_moving))
+    val progress by animateLottieCompositionAsState(composition)
+    LottieAnimation(
+        modifier = modifier.height(124.dp),
+        composition = composition,
+        progress = { progress },
+    )
+}
+@Composable
+fun TopicHeader(modifier: Modifier) {
+    Text(
+        modifier = modifier,
+        text = stringResource(id = R.string.topics_title),
+        style = MaterialTheme.typography.displaySmall
+    )
+}
 
+@Composable
+fun TopicList(modifier: Modifier = Modifier, topics: List<Topic>) {
+    LazyColumn(
+        modifier = modifier
+            .wrapContentSize()
+            .widthIn(320.dp, 600.dp)
+            .padding(32.dp),
+    ) {
+        items(
+            items = topics,
+            key = { it.id }
+        ) { topic ->
+            ElevatedButton(
+                modifier = Modifier
+                    .animateItemPlacement()
+                    .padding(vertical = 4.dp)
+                    .fillMaxWidth(),
+                shape = CutCornerShape(4.dp),
+                border = BorderStroke(1.dp, MaterialTheme.colorScheme.outlineVariant),
+                onClick = {},
+            ) {
+                Text(text = topic.title, style = MaterialTheme.typography.headlineMedium)
+            }
         }
     }
 }
--- app/src/main/java/de/yanos/islam/util/CustomViews.kt
+++ app/src/main/java/de/yanos/islam/util/CustomViews.kt
@@ -1,13 +1,56 @@
 package de.yanos.islam.util
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.ImageShader
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
+import de.yanos.islam.R
 
 @Composable
 @Preview
 fun IslamDivider() {
     Divider(modifier = Modifier.alpha(0.2f))
+}
+
+@Composable
+fun PatternedBackgroung(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        BackGroundPattern(modifier = Modifier.matchParentSize())
+        content()
+    }
+}
+
+
+@Composable
+fun BackGroundPattern(modifier: Modifier = Modifier) {
+    val pattern = ImageBitmap.imageResource(R.drawable.pattern_2)
+    Canvas(
+        modifier = modifier.alpha(0.1F)
+    ) {
+        val paint = Paint().asFrameworkPaint().apply {
+            isAntiAlias = true
+            shader = ImageShader(pattern, TileMode.Repeated, TileMode.Repeated)
+        }
+
+        drawIntoCanvas {
+            it.nativeCanvas.drawPaint(paint)
+        }
+        paint.reset()
+    }
 }
